34f17c733bb74c882cbe125cf7d4bdc19848ddb0
Christopher Barrett <christopherbarrettza@gmail.com>
2016-07-28 08:10:32 +0200

    pom setup using the default archetype

---
+++ BeerApp/src/main/java/com/christopher/beerapp/App.java
@@ -0,0 +1,13 @@
+package com.christopher.beerapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
